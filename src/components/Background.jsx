4e62df88d24bb97a6c16bedeb4af3a97cae11adc
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-26 14:46:15 +0530

    Gallery

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,8 +4,8 @@ function Background() {
     return (
         <>
             <div className=' fixed z-[2] w-full h-screen '>
-                <div className=' absolute w-full py-10  top-[5%] flex justify-center font-semibold text-zinc-600 text-xl '> Documents.</div>
-                <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none  font-semibold text-zinc-900'  >Docs.</h1>
+                <div className=' absolute w-full py-10  top-[5%] flex justify-center font-semibold text-zinc-600 text-xl '> Gallery.</div>
+                <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none  font-semibold text-zinc-900'  >Gallery.</h1>
             </div>
 
         </>
